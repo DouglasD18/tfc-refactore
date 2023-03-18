654ff160a6794dd98b84b4f74463c3ef264826d6
Douglinhas <daguiaralcantara@gmail.com>
2023-03-18 13:36:27 -0300

    test: ensure LoginController calls ValidateLoginBody with correct values

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,17 +1,48 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { LoginController } from './login';
+import { ValidateLoginBody } from "../../protocols/validate-login-body";
+import { Login } from "../../../domain/models/login";
+import { HttpRequest } from '../../protocols/http';
 
-const makeSut = (): LoginController => {
-  return new LoginController();
+const httpRequestStub: HttpRequest = {
+  body: {
+    email: "valid@mail.com",
+    password: "valid_password"
+  }
+}
+
+const makeValidateLoginBodyStub = (): ValidateLoginBody => {
+  class ValidateLginBodyStub implements ValidateLoginBody {
+    validate(body: Login): boolean | string[] {
+      return true;
+    }
+  }
+
+  return new ValidateLginBodyStub();
+}
+
+interface typesSut {
+  sut: LoginController
+  validateLoginBodyStub: ValidateLoginBody
+}
+
+const makeSut = (): typesSut => {
+  const validateLoginBodyStub = makeValidateLoginBodyStub();
+  const sut = new LoginController(validateLoginBodyStub);
+
+  return {
+    sut,
+    validateLoginBodyStub
+  }
 }
 
 describe("LoginController", () => {
-  test("Should return 400 if email is provided", async () => {
-    const sut = makeSut();
-    const httpRequest = {}
+  it("Should call ValidateLoginBody with correct values", async () => {
+    const { sut, validateLoginBodyStub } = makeSut();
 
-    const httpResponse = await sut.handle(httpRequest);
+    const validSpy = vi.spyOn(validateLoginBodyStub, "validate");
+    await sut.handle(httpRequestStub);
 
-    expect(httpResponse.statusCode).toBe(400);
+    expect(validSpy).toHaveBeenCalledWith(httpRequestStub.body);
   })
 })
