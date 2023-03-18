34489a5ccdda9946b36cf2bcdb596d19798a27bc
Douglinhas <daguiaralcantara@gmail.com>
2023-03-18 13:45:36 -0300

    test: ensure LoginController return 400 when ValidateLoginBody return a array

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,8 @@ import { LoginController } from './login';
 import { ValidateLoginBody } from "../../protocols/validate-login-body";
 import { Login } from "../../../domain/models/login";
 import { HttpRequest } from '../../protocols/http';
+import { MissingParamError } from '../../errors/missing-param-error';
+import { InvalidParamError } from "../../errors";
 
 const httpRequestStub: HttpRequest = {
   body: {
@@ -44,5 +46,45 @@ describe("LoginController", () => {
     await sut.handle(httpRequestStub);
 
     expect(validSpy).toHaveBeenCalledWith(httpRequestStub.body);
+  })
+
+  it("Should return MissingParam email error if with ValidateLoginBody returns", async () => {
+    const { sut, validateLoginBodyStub } = makeSut();
+
+    vi.spyOn(validateLoginBodyStub, "validate").mockReturnValueOnce(["MissingParam", "email"])
+    const httpResponse = await sut.handle(httpRequestStub);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  })
+
+  it("Should return MissingParam password error if with ValidateLoginBody returns", async () => {
+    const { sut, validateLoginBodyStub } = makeSut();
+
+    vi.spyOn(validateLoginBodyStub, "validate").mockReturnValueOnce(["MissingParam", "password"])
+    const httpResponse = await sut.handle(httpRequestStub);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  })
+
+  it("Should return InvalidParam email error if with ValidateLoginBody returns", async () => {
+    const { sut, validateLoginBodyStub } = makeSut();
+
+    vi.spyOn(validateLoginBodyStub, "validate").mockReturnValueOnce(["InvalidParam", "email", "Email must be a string!"])
+    const httpResponse = await sut.handle(httpRequestStub);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError("email", "Email must be a string!"));
+  })
+
+  it("Should return InvalidParam password error if with ValidateLoginBody returns", async () => {
+    const { sut, validateLoginBodyStub } = makeSut();
+
+    vi.spyOn(validateLoginBodyStub, "validate").mockReturnValueOnce(["InvalidParam", "password", "Password must be a string with length greater than 5"])
+    const httpResponse = await sut.handle(httpRequestStub);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError("password", "Password must be a string with length greater than 5"));
   })
 })
